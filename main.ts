ba046a4adfe82a4b09875939d73173e52ab3f4c7
s21110 <122764722+s21110@users.noreply.github.com>
2023-03-14 10:53:49 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.South)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+})
 basic.forever(function () {
 	
 })
